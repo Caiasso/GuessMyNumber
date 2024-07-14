0dd7a0a6f713e5cfe48c8ad365fd1fb19ed60034
Caiasso <guilhermecaiasso@gmail.com>
2024-07-14 15:40:24 -0300

    Refactored stuff

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber;
 let score = 20;
 let highscore = 0;
 let winner = false;
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector(`.guess`).value);
+generateSecretNumber();
 
-    if(score > 0 && !winner && guess < 21)
-    {
-        if(!guess)
-        {
-            document.querySelector(`.message`).textContent = `⛔ No number!`;
-        }
-        else if(guess === secretNumber)
-        {
-            document.querySelector(`.message`).textContent = `🎉 Correct number!`;
-            document.querySelector(`body`).style.backgroundColor = `green`;
-            document.querySelector('.number').style.width = '30rem';
-            document.querySelector('.number').textContent = secretNumber;
-            winner = true;
-        }
-        else if(guess > secretNumber)
-        {
-            document.querySelector(`.message`).textContent = `📈 Too high!`;
-            score --;
-            document.querySelector(`.score`).textContent = score;
-        }
-        else
-        {
-            document.querySelector(`.message`).textContent = `📉 Too low!`;
-            score --;
-            document.querySelector(`.score`).textContent = score;
-        }
+const displayMessage = function (message) {
+  document.querySelector(`.message`).textContent = message;
+};
+
+const changeScore = function () {
+  document.querySelector(`.score`).textContent = score;
+};
+
+const generateSecretNumber = function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+};
+
+const backgroundColor = function (color) {
+  document.querySelector(`body`).style.backgroundColor = color;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+
+  if (score && !winner && guess < 21) {
+    if (!guess || guess < 0) {
+      displayMessage(`⛔ No number!`);
+    } else if (guess === secretNumber) {
+      displayMessage(`🎉 Correct number!`);
+      backgroundColor(`green`);
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = secretNumber;
+      winner = true;
+    } else {
+      displayMessage(guess > secretNumber ? `📈 Too high!` : `📉 Too low!`);
+      score--;
+      changeScore();
     }
+  } else if (score < 1 && !winner) {
+    displayMessage(`💥 You lost the game!`);
+    document.querySelector(`.score`).textContent = 0;
+  }
+});
 
-    else if(score < 1 && !winner){
-        document.querySelector(`.message`).textContent = `💥 You lost the game!`;
-        document.querySelector(`.score`).textContent = 0;
-    }
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  if (winner) {
+    highscore =
+      score > Number(document.querySelector(`.highscore`).textContent)
+        ? score
+        : highscore;
+    winner = false;
+  }
+  score = 20;
+  generateSecretNumber;
+  changeScore();
+  displayMessage(`Start guessing...`);
+  document.querySelector(`.number`).textContent = '?';
+  backgroundColor(`#222`);
+  document.querySelector(`.guess`).value = '';
+  document.querySelector('.number').style.width = '15rem';
 
-})
-
-document.querySelector(`.again`).addEventListener(`click`, function(){
-    
-    if(winner)
-    {
-        highscore = score > Number(document.querySelector(`.highscore`).textContent) ? score : highscore;
-        winner = false;
-    }
-    score = 20;
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector(`.score`).textContent = score;
-    document.querySelector(`.message`).textContent = `Start guessing...`;
-    document.querySelector(`.number`).textContent = '?';
-    document.querySelector(`body`).style.backgroundColor = `#222`;
-    document.querySelector(`.guess`).value = '';
-    document.querySelector('.number').style.width = '15rem';
-
-
-    document.querySelector(`.highscore`).textContent = highscore;
-    
-})
+  document.querySelector(`.highscore`).textContent = highscore;
+});
